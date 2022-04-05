9e7d830e7f148dbb0bbeb0623719b7248b07980f
Bima Santrya <hafidhbaru@gmail.com>
2022-04-06 02:15:36 +0700

    add post delete & patch route

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ const { connectDB, getDB } = require('./config/db')
 // init express app
 const app = express()
 const PORT = process.env.PORT || 4100
+app.use(express.json())
 
 // db connection
 let db
@@ -21,6 +22,7 @@ connectDB((err) => {
 
 // routes
 app.get('/', (req, res) => res.send('Hello Node JS'))
+// get all the books
 app.get('/books', async (req, res) => {
   try {
     let books = []
@@ -34,6 +36,7 @@ app.get('/books', async (req, res) => {
     return res.status(400).json({ msg: err.message })
   }
 })
+// get single book by id
 app.get('/books/:id', async (req, res) => {
   try {
     const bookID = req.params.id
@@ -47,6 +50,54 @@ app.get('/books/:id', async (req, res) => {
   catch(err) {
     console.log(err.message)
     return res.status(400).json({ msg: err.message })
+  }
+})
+// add new book
+app.post('/books', async (req, res) => {
+  try {
+    const bookDB = db.collection('books')
+    const book = req.body
+    const addBook = await bookDB.insertOne(book)
+
+    return res.status(201).json(addBook)
+  }
+  catch(err) {
+    console.log(err.message)
+    return res.status(400).json({ msg: err.message })
+  }
+})
+// delete one book by id
+app.delete('/books/:id', async (req, res) => {
+  try {
+    const bookID = req.params.id
+    if (ObjectId.isValid(bookID)) {
+      const Book = db.collection('books')
+      const bookData = await Book.deleteOne({ _id: ObjectId(bookID) })
+
+      return res.status(201).json(bookData)
+    } else return res.status(500).json({ error: 'the ID is not valid' })
+  }
+  catch(err) {
+    console.log(err.message)
+    return res.status(400).json({ msg: err.message })
+  }
+})
+// update single book
+app.patch('/books/:id', async (req, res) => {
+  const bookID = req.params.id
+  const updates = req.body
+  const Book = db.collection('books')
+
+  try {
+    if (ObjectId.isValid(bookID)) {
+      const updateBook = await Book.updateOne({ _id: ObjectId(bookID) }, {$set: updates})
+
+      res.status(201).json(updateBook)
+    } else res.status(500).json({ error: 'the ID is not valid' })
+  }
+  catch(err) {
+    console.log(err.message)
+    res.status(400).json({ msg: err.message })
   }
 })
 
